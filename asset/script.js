f9a2c88c6a3a6bb727154ccf885435f6bf8a9854
YipChiSan <57831867+YipChiSan@users.noreply.github.com>
2022-06-29 12:14:33 +1000

    Button can save values

--- asset/script.js
+++ asset/script.js
@@ -1,6 +1,6 @@
 
-
-let button = $("button");
+let searchBarEl = $("#city-search");
+let buttonEl = $("button");
 let todayWeatherEl = $("#today-weather");
 let weatherForecastEl = $("#weather-forecast");
 const apiKey = "083d29d9792e86f3b636dd9ed82e9c2b";
@@ -8,10 +8,16 @@ const weatherBaseUrl = "https://api.openweathermap.org/data/2.5/onecall?";
 const locBaseUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
 let lat;
 let lon;
-let address = "new york";
+let address;
 
-function handleSubmit() {
+buttonEl.click(handleSubmit);
 
+function handleSubmit(e) {
+    e.preventDefault();
+    let curVal = searchBarEl.val();
+    if (curVal) {
+        address = curVal;
+    }
 }
 
 function fetchWeatherData() {
@@ -23,7 +29,8 @@ function fetchWeatherData() {
 function fetchLocData() {
     fetch(locBaseUrl + address + "&limit=1&appid=" + apiKey)
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => {
+            lat = data[0].lat;
+            lon = data[0].lon;
+        });
 }
-
-fetchLocData();
