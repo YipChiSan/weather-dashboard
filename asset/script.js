92997882f4983511bebd2cafb14a7819f1b893e8
YipChiSan <57831867+YipChiSan@users.noreply.github.com>
2022-06-29 10:13:18 +1000

    Able to get the data from the remote server

--- asset/script.js
+++ asset/script.js
@@ -1,3 +1,19 @@
 let button = $("button");
 let todayWeatherEl = $("#today-weather");
 let weatherForecastEl = $("#weather-forecast");
+const apiKey = "083d29d9792e86f3b636dd9ed82e9c2b";
+const baseUrl = "https://api.openweathermap.org/data/2.5/onecall?"; 
+let lat = "33.44";
+let lon = "-94.04";
+
+function handleSubmit() {
+
+}
+
+function fetchData() {
+    fetch(baseUrl + "lat="+ lat + "&lon=" + lon + "&appid=" + apiKey)
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+
+fetchData();
