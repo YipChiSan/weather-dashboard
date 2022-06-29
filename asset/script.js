21f3449c83277dfc037bb4d7aebbbb5c1b8823ea
YipChiSan <57831867+YipChiSan@users.noreply.github.com>
2022-06-29 11:58:53 +1000

    Set up the basic api call to get location coordinates

--- asset/script.js
+++ asset/script.js
@@ -1,19 +1,29 @@
+
+
 let button = $("button");
 let todayWeatherEl = $("#today-weather");
 let weatherForecastEl = $("#weather-forecast");
 const apiKey = "083d29d9792e86f3b636dd9ed82e9c2b";
-const baseUrl = "https://api.openweathermap.org/data/2.5/onecall?"; 
-let lat = "33.44";
-let lon = "-94.04";
+const weatherBaseUrl = "https://api.openweathermap.org/data/2.5/onecall?"; 
+const locBaseUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
+let lat;
+let lon;
+let address = "new york";
 
 function handleSubmit() {
 
 }
 
-function fetchData() {
-    fetch(baseUrl + "lat="+ lat + "&lon=" + lon + "&appid=" + apiKey)
+function fetchWeatherData() {
+    fetch(weatherBaseUrl + "lat="+ lat + "&lon=" + lon + "&appid=" + apiKey)
         .then(response => response.json())
         .then(data => console.log(data));
 }
 
-fetchData();
+function fetchLocData() {
+    fetch(locBaseUrl + address + "&limit=1&appid=" + apiKey)
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+
+fetchLocData();
