3ee626f89c39d944640ebfa62c39a19ec07d9ee2
YipChiSan <zye39@yahoo.com>
2022-07-03 16:40:42 +1000

    Store data in state with correct format

--- asset/script.js
+++ asset/script.js
@@ -1,3 +1,6 @@
+let lat;
+let lon;
+let address;
 let searchHistory = $("#search-history");
 let searchBarEl = $("#city-search");
 let buttonEl = $("button");
@@ -6,9 +9,7 @@ let weatherForecastEl = $("#weather-forecast");
 const apiKey = "083d29d9792e86f3b636dd9ed82e9c2b";
 const weatherBaseUrl = "https://api.openweathermap.org/data/2.5/onecall?"; 
 const locBaseUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
-let lat;
-let lon;
-let address;
+const weatherData = [];
 
 buttonEl.click(handleSubmit);
 displaySearchHistory();
@@ -28,10 +29,7 @@ function handleSubmit(e) {
     e.preventDefault();
     getSearchHistory();
     displaySearchHistory();
-}
-
-function getWeatherData() {
-    
+    fetchData();
 }
 
 function getSearchHistory() {
@@ -49,16 +47,33 @@ function getSearchHistory() {
 }
 
 function fetchWeatherData() {
-    fetch(weatherBaseUrl + "lat="+ lat + "&lon=" + lon + "&appid=" + apiKey)
+    fetch(weatherBaseUrl + "lat="+ lat + "&lon=" + lon + "&appid=" + apiKey + "&units=metric")
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => handleWeatherData(data));
 }
 
-function fetchLocData() {
+function fetchData() {
     fetch(locBaseUrl + address + "&limit=1&appid=" + apiKey)
         .then(response => response.json())
         .then(data => {
             lat = data[0].lat;
             lon = data[0].lon;
+            fetchWeatherData();
         });
+}
+
+function handleWeatherData(data) {
+    for (let i = 0; i < 6; i++) {
+        weatherData.push({});
+        weatherData[weatherData.length - 1]["date"] = moment.unix(data.daily[i].dt).format('D/M/YYYY');
+        weatherData[weatherData.length - 1]["icon"] = data.daily[i].weather[0].icon;
+        weatherData[weatherData.length - 1]["temp"] = data.daily[i].temp.day;
+        weatherData[weatherData.length - 1]["wind"] = data.daily[i].wind_speed;
+        weatherData[weatherData.length - 1]["humidity"] = data.daily[i].humidity;
+        weatherData[weatherData.length - 1]["uvIndex"] = data.daily[i].uvi;
+    }
+}
+
+function displayTodayWeather() {
+
 }
